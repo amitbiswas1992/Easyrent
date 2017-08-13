0caa32742a8836ddf22470f0760af2bfa5cce5cd
Amit Biswas <biswas1992@hotmail.com>
2017-08-13 04:47:49 -0400

    Test commit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,10 @@
 class PortfoliosController < ApplicationController
 def index
 @portfolio_item=Portfolio.all
-end 
-def new 
+end
+def new
 @portfolio_item=Portfolio.new
-end 
+end
 
 ## set the  create  method and set the params --AB
  def create
@@ -17,14 +17,14 @@ end
       else
         format.html { render :new }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-       
+
       end
     end
   end
 
-def edit 
+def edit
  @portfolio_item=Portfolio.find(params[:id])
-    end 
+    end
 
 ## create updating method and set the params --AB
 def update
@@ -41,18 +41,18 @@ def update
     end
   end
 
-def show 
+def show
  @portfolio_item=Portfolio.find(params[:id])
 end
-def destroy 
-  ##This is perform the look up 
+def destroy
+  ##This is perform the look up
  @portfolio_item=Portfolio.find(params[:id])
-  ##destroy and delete record 
+  ##destroy and delete record
    @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
       format.json { head :no_content }
     end
-  end 
+  end
 
 end
